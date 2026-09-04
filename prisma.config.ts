479271246e275084f91e99ec77c7354907c426ef
Carvalh0Henrique <carvalhoghenrique@gmail.com>
2026-09-03 20:27:44 -0600

    chore: ajusta dependencias

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   schema: "src/prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+    seed: "node --loader ts-node/esm src/prisma/seed.ts",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
